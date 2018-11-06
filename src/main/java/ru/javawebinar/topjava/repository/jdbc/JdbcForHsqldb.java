d8dabdd5fcab3efa24a4d1933e914d59f8e50d76
indisbk <32897798+indisbk@users.noreply.github.com>
2018-11-06 13:49:50 +0800

    HW05 5 item is done.

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcForHsqldb.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcForHsqldb.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -8,30 +9,30 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
-
 @Repository
-@Profile("postgres")
-public class JdbcMealRepositoryImpl extends AbstractJdbcMealRepository {
-
+@Profile("hsqldb")
+public class JdbcForHsqldb extends AbstractJdbcMealRepository {
     @Autowired
-    public JdbcMealRepositoryImpl(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+    public JdbcForHsqldb(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
         super(jdbcTemplate, namedParameterJdbcTemplate);
     }
 
     @Override
     protected MapSqlParameterSource createMap(Meal meal, int userId) {
+        var tm = Timestamp.valueOf(meal.getDateTime());
         return new MapSqlParameterSource()
                 .addValue("id", meal.getId())
                 .addValue("description", meal.getDescription())
                 .addValue("calories", meal.getCalories())
-                .addValue("date_time", meal.getDateTime())
+                .addValue("date_time", tm)
                 .addValue("user_id", userId);
     }
 
     @Override
-    protected LocalDateTime getValue(LocalDateTime date) {
-        return date;
+    protected Timestamp getValue(LocalDateTime date) {
+        return Timestamp.valueOf(date);
     }
 }
